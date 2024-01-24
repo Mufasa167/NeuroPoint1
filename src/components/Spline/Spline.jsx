3321504cb23d96a3b43fdc35d04fb34b54c77575
Faiez Kaddan <goodkidmcf@Faiezs-MacBook-Pro.local>
2024-01-25 01:33:57 +0200

    frontend commit

--- src/components/Spline/Spline.jsx
+++
@@ -1,6 +0,0 @@
-import Spline from '@splinetool/react-spline';
-export default function App() {
-  return (
-    <Spline scene="https://prod.spline.design/Hu1mUegv6apklXn1/scene.splinecode" />
-  );
-}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import App from "../components/Spline/Spline.jsx"
+
 import ContactForm from '../components/ContactForm/ContactForm.jsx'
 const Contact = () => {
   return (
     <div>
-        <div className="ml-auto"><App/></div>
-    
-      <div className='mr-auto'>
-        <ContactForm/></div>
+        
+        <ContactForm/>
       
         </div>
   )
